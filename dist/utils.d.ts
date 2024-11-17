5c9cee7576a90ff48a7c4bf97409e68c257d3c91
demiavaliani <demikoavaliani@gmail.com>
2024-11-17 21:00:29 +0200

    refactor

--- dist/utils.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Typer {
-    private elementid;
-    private actionQueue;
-    constructor(elementid: string);
-    type(text: string, delay?: number): this;
-    go(): Promise<void>;
-}
-//# sourceMappingURL=utils.d.ts.map
