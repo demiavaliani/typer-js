e12c8d94f417c1a1ec86a32d307f4598c59ea21b
demiavaliani <demikoavaliani@gmail.com>
2024-11-28 12:00:35 +0200

    TEMP

---
+++ move this folder to a separate root out of package folder. this is just for trying out the actual code in utils.ts/index.js
@@ -0,0 +1,10 @@
+import { Typer } from "typer-js";
+const typer = new Typer("text-box");
+// typer.type("DEMI", 300).moveCaret(4).go();
+typer.type("DEMI", 300).go();
+// window.setTimeout(() => {
+// 	typer.moveCaret(3).go();
+// }, 3000);
+// window.setTimeout(() => {
+// 	typer.moveCaret(2).go();
+// }, 5000);
